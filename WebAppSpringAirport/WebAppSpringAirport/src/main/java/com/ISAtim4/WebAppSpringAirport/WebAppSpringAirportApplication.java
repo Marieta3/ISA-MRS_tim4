62747ded718726780f55d66cabbe772f6595e016
NikolaMax <nikolamax015@gmail.com>
2019-03-23 16:02:15 +0100

    Inicijalni (starter) projekat

---
+++ WebAppSpringAirport/WebAppSpringAirport/src/main/java/com/ISAtim4/WebAppSpringAirport/WebAppSpringAirportApplication.java
@@ -0,0 +1,13 @@
+package com.ISAtim4.WebAppSpringAirport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppSpringAirportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppSpringAirportApplication.class, args);
+	}
+
+}
