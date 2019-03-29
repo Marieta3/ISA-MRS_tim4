5b73b36a8bc54ddfd65b2fc40642a2d7cdf27c83
Nikola Maksimovic <nikolamax015@gmail.com>
2019-03-29 20:00:02 +0100

    Povezivanje inicijalne baze podataka za hotel,sobe,usluge i dodavanje na bazu

--- WebAppSpringAirport/WebAppSpringAirport/src/main/resources/data.sql
+++ WebAppSpringAirport/WebAppSpringAirport/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into hotel(adresa,naziv,ocena,opis) values ('nekaAdresa','nekiNaziv',10,'nekiOpis');
-insert into hotel(adresa,naziv,ocena,opis) values ('nekaAdresa2','nekiNaziv2',11,'nekiOpis2');
-insert into usluga(cena,opis,hotel_id) values (100,'opis',1);
-insert into usluga(cena,opis,hotel_id) values (200,'opis2',2);
-insert into sobe(broj_kreveta,ocena,opis,hotel_id) values (3,9.9,'nekiOpisss',1);
-insert into sobe(broj_kreveta,ocena,opis,hotel_id) values (5,8.8,'nekiOpisss2',1);
+insert into hotel(id,adresa,naziv,ocena,opis) values (1,'nekaAdresa','nekiNaziv',10,'nekiOpis');
+insert into hotel(id,adresa,naziv,ocena,opis) values (2,'nekaAdresa2','nekiNaziv2',11,'nekiOpis2');
+insert into usluga(id,cena,opis,hotel_id) values (1,100,'opis',1);
+insert into usluga(id,cena,opis,hotel_id) values (2,200,'opis2',2);
+insert into soba(id,broj_kreveta,ocena,opis,hotel_id) values (1,3,9.9,'nekiOpisss',1);
+insert into soba(id,broj_kreveta,ocena,opis,hotel_id) values (2,5,8.8,'nekiOpisss2',1);
 
